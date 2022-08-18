5b7c6da65ad7b2fc3532c5d0c25f3a475502511d
shingie <72065045+ShingiraiBhengesa@users.noreply.github.com>
2022-08-18 18:14:53 +0300

    update changes

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,8 +1,40 @@
+const tasks = [
+  {
+    id: 1,
+    text: "Doctors Appointment",
+    day: "Feb 5th at 2:30pm",
+    reminder: true,
+  },
 
-const Tasks = () => {
+  {
+    id: 2,
+    text: "Grocery Shopping",
+    day: "Feb 28th at 1:00pm",
+    remider: false,
+  },
+
+  {
+    id: 3,
+    text:"Gardening",
+    day: "May 1st at 3:00pm",
+    reminder: true,
+  },
+
+  {
+    id: 4,
+    text: "School Run",
+    day:"May 2nd 8:00am",
+    reminder: false,
+  }
+
+
+]
+
+
+export const Tasks = () => {
   return (
-    <div>Tasks</div>
-  )
-}
+    <>
+      {tasks.map}
+    </>)
 
-export default Tasks
+}
